a00cadd8a0656ce14c0f6a47b7afebac110f79f7
Antlion12 <antlion12@protonmail.com>
2021-12-26 23:14:09 -0800

    Separated update and alert tasks.

--- discord_bot.py
+++ discord_bot.py
@@ -20,12 +20,29 @@ from absl import flags
 from debt_lib import DebtTracker
 from discord.ext import tasks
 from pathlib import Path
+from typing import List
+from typing import Tuple
 
+import asyncio
 import discord
 import json
+import queue
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string("config", "config.json", "JSON file containing bot config.")
+
+
+class Tracker(object):
+    def __init__(self, tracker: DebtTracker, message: str):
+        self.tracker = tracker
+        self.message = message
+
+
+# An Alert specifies to which channel to send a message.
+class Alert(object):
+    def __init__(self, channel_name: str, message: str):
+        self.channel_name = channel_name
+        self.message = message
 
 
 class AntlionDeFiBot(discord.Client):
@@ -35,15 +52,22 @@ class AntlionDeFiBot(discord.Client):
         # Set of trackers to update on each loop of the background task.
         self._trackers = []
 
+        self._alerts_queue = queue.Queue()
+
         # Set of channels to send alerts to.
         assert "config" in kwargs
         self._config_file = kwargs["config"]
         self.load_config()
 
-        self._force_update = False
+        # Start update task.
+        self.update_task.start()
+        self.alert_task.start()
 
-        # Start background task.
-        self.my_background_task.start()
+    def add_tracker(self, tracker: DebtTracker):
+        _, message = tracker.get_current()
+        self._trackers.append(Tracker(tracker, message))
+        print("Current debt position for tracker:")
+        print(self._trackers[-1].message)
 
     def load_config(self):
         assert Path(self._config_file).is_file()
@@ -64,11 +88,17 @@ class AntlionDeFiBot(discord.Client):
     def channel_is_subscribed(self, channel_name) -> bool:
         return channel_name in self._config["channels"]
 
-    def get_subscribed_channels(self) -> dict:
-        return self._config["channels"].items()
+    def get_subscribed_channels(self) -> List[str]:
+        return self._config["channels"].keys()
+
+    def get_channel_id_by_name(self, name: str) -> int:
+        return self._config["channels"][name]
 
     def get_token(self) -> str:
         return self._config["token"]
+
+    def schedule_alert(self, channel_name, message):
+        self._alerts_queue.put(Alert(channel_name, message))
 
     async def on_ready(self):
         print(f"""Logged in as {self.user.name}#{self.user.discriminator}""")
@@ -83,6 +113,7 @@ class AntlionDeFiBot(discord.Client):
 
             if self.channel_is_subscribed(channel_name):
                 print(f"""User {message.author} requested update.""")
+                await message.channel.send(f"""@{message.author} requested an update. Coming right up....""")
             else:
                 # This is a new subscription.
                 self.subscribe_to_channel(channel_name, channel_id)
@@ -91,38 +122,48 @@ class AntlionDeFiBot(discord.Client):
                 await message.channel.send("gm")
                 await message.channel.send("You have subscribed to updates from the Antlion DeFi Bot.")
                 print(f"""Subscribed to {channel_name} ({channel_id})""")
-            self._force_update = True
-            await self.my_background_task()
+
+            # Schedule alert for this channel containing current messages.
+            for tracker in self._trackers:
+                self.schedule_alert(channel_name, tracker.message)
 
     @tasks.loop(seconds=300)
-    async def my_background_task(self):
-        print("Running background task.")
-        if not self.get_subscribed_channels():
-            print("No subscribed channels. Skipping update.")
-            return
-
-        # Run each tracker. and message the channel if the update raises an
-        # alert.
+    async def update_task(self):
+        print("Running update task.")
         for tracker in self._trackers:
-            has_alert, message = tracker.update()
+            await asyncio.sleep(0)
+            has_alert, message = tracker.tracker.update()
+            await asyncio.sleep(0)
+            tracker.message = message
+            print(f"""Updated tracker for {tracker.tracker.get_name()} with the following message:""")
             print(message)
-            if not has_alert and not self._force_update:
-                continue
-            self._force_update = False
 
-            # We have an alert to send.
-            for channel_name, channel_id in self.get_subscribed_channels():
-                channel = self.get_channel(channel_id)
-                await channel.send(message)
-                print(f"""Sent alert to {channel_name}.""")
+            if has_alert:
+                # Raise alerts only if we have subscribed to channels.
+                for channel_name in self.get_subscribed_channels():
+                    self.schedule_alert(channel_name, message)
 
+    @update_task.before_loop
+    async def before_update_task(self):
+        # Wait for bot to log in.
+        await self.wait_until_ready()
+        await asyncio.sleep(20)
 
-    @my_background_task.before_loop
-    async def before_my_task(self):
-        await self.wait_until_ready()  # Wait for bot to log in.
+    @tasks.loop(seconds=10)
+    async def alert_task(self):
+        queue = self._alerts_queue
+        print(f"""Running alert task with {queue.qsize()} alerts.""")
+        while not queue.empty():
+            alert = queue.get()
+            channel = self.get_channel(self.get_channel_id_by_name(alert.channel_name))
+            await channel.send(alert.message)
+            print(f"""Sent alert to {alert.channel_name} with the following message:""")
+            print(alert.message)
+            queue.task_done()
 
-    def add_tracker(self, tracker: DebtTracker):
-        self._trackers.append(tracker)
+    @alert_task.before_loop
+    async def before_alert_task(self):
+        await self.wait_until_ready()
 
 
 def main(argv):
